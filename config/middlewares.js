ca5c2623606847d9424a798dcdd6a7e142821a92
Michael Kim <michael.yeob@gmail.com>
2025-12-13 13:21:43 -0800

    Update CORS config to allow Vercel frontend

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,19 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://unroasted.vercel.app',
+        'https://www.unroasted.vercel.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
