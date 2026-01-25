6d9d2b3b94ac9f87e6469de48b820d010dc0ee4a
Michael Kim <michael.yeob@gmail.com>
2026-01-24 16:36:53 -0800

    plugin changed

--- config/plugins.js
+++ config/plugins.js
@@ -4,18 +4,24 @@ module.exports = ({ env }) => ({
       jwtSecret: env('JWT_SECRET'),
     },
   },
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  },
+  // Only use Cloudinary if credentials are provided (production)
+  // For local development without Cloudinary, it will use local file storage
+  ...(env('CLOUDINARY_NAME') && env('CLOUDINARY_KEY') && env('CLOUDINARY_SECRET')
+    ? {
+        upload: {
+          config: {
+            provider: 'cloudinary',
+            providerOptions: {
+              cloud_name: env('CLOUDINARY_NAME'),
+              api_key: env('CLOUDINARY_KEY'),
+              api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+              upload: {},
+              delete: {},
+            },
+          },
+        },
+      }
+    : {}),
 });
