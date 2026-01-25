4014d833a5edc8d16b7af4544378e39f308bb44b
Michael Kim <michael.yeob@gmail.com>
2026-01-24 16:56:49 -0800

    Fix CORS configuration error

--- config/middlewares.js
+++ config/middlewares.js
@@ -19,21 +19,12 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ({ env }) => {
-        const origins = [
-          'http://localhost:3000',
-          'http://localhost:3002',
-          'https://unroasted.vercel.app',
-          'https://www.unroasted.vercel.app',
-        ];
-        
-        // Add custom origin from environment variable if provided
-        if (env('CLIENT_URL')) {
-          origins.push(env('CLIENT_URL'));
-        }
-        
-        return origins;
-      },
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:3002',
+        'https://unroasted.vercel.app',
+        'https://www.unroasted.vercel.app',
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
